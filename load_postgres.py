a0f1c9f341ad514432f19d8f0996a72284839c1a
Yuvankfaujdar <135536996+Yuvankfaujdar@users.noreply.github.com>
2026-03-06 10:57:10 +0530

    Update load_postgres.py

--- load_postgres.py
+++ load_postgres.py
@@ -1,10 +1,14 @@
 import pandas as pd
 import psycopg2
-import os
+
+DATABASE_URL = "postgresql://postgres:uoUtsOtifvCDUjUOordSOAEUJwpCGsXF@interchange.proxy.rlwy.net:23643/railway"
 
 df = pd.read_csv("transactions.csv")
 
-conn = psycopg2.connect(os.getenv("DATABASE_URL"))
+# Convert date format
+df["created_at"] = pd.to_datetime(df["created_at"], format="%d-%m-%Y %H:%M")
+
+conn = psycopg2.connect(DATABASE_URL)
 cursor = conn.cursor()
 
 cursor.execute("""
@@ -17,11 +21,21 @@ created_at TIMESTAMP
 )
 """)
 
-for _,row in df.iterrows():
+for _, row in df.iterrows():
 
     cursor.execute("""
     INSERT INTO transactions VALUES (%s,%s,%s,%s,%s)
-    """,(row.user_id,row.payer,row.amount,row.currency,row.created_at))
+    """,(
+        row["user_id"],
+        row["payer"],
+        row["amount"],
+        row["currency"],
+        row["created_at"]
+    ))
 
 conn.commit()
+
+cursor.close()
 conn.close()
+
+print("Data successfully loaded into Postgres")
